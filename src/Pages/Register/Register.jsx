0bac42767b738bf40faecd9e232da9a5c742c4aa
Ahmed badr <abobader862@gmail.com>
2025-03-17 16:43:22 +0200

    resolve config

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { auth , createUserWithEmailAndPassword , db , setDoc , doc} from "../../firebase/firebase";
+import { useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
 
 
 function Register() {
@@ -109,9 +111,16 @@ function Register() {
  
 const  createUser =(uid) => {
     
-
+  const userDeatails = {
+      name: userUpData.name,
+      email: userUpData.email,
+      usrName: userUpData.usrName,
+      password: userUpData.password,
+      confPassword: userUpData.confPassword,
+      allDishes: allDishes.wishlist,
+    };
   
-    setDoc(doc(db, "users2", uid), userUpData)
+    setDoc(doc(db, "users2", uid), userDeatails)
   
       .then(() => {
         console.log("User created successfully!");
