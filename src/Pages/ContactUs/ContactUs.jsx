d6b8e63fe993b88f87c479518a3ef37e3ad64583
Ahmed badr <abobader862@gmail.com>
2025-04-09 14:07:46 +0200

    chat with ai

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,5 +1,9 @@
 import { useSelector } from "react-redux";
 import Chat from "../../Components/Chat_Box/chat";
+// import FirestoreData from "../../Components/Chat_AI/FirestoreData";
+import Chat_AI from "../../Components/Chat_AI/ChatAi";
+import FirestoreData from "../../Components/Chat_AI/FirestoreData";
+// import BotComponent from "../../Components/Chat_AI/ChatAi";
 
 function ContactUs() {
   const userState55 = useSelector((state) => state.UserData['UserState']);
@@ -14,9 +18,10 @@ console.log(userState55.uid);
         uidChats = {userState55.uid}
         showChat = {true}
         />
-       </div>
+        <Chat_AI/>
         
-        
+        <FirestoreData/>
+        </>
     );
 
 }                
